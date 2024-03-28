645901f610fa5dae461a8bcedf2332a4034e6d36
Jimena Hernández <her21199@uvg.edu.gt>
2024-03-28 16:51:52 -0600

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,26 +1,11 @@
-"use client"
-import { useEffect, useState } from "react"
+import React from "react"
 import FormLogin from "@/components/FormLogin/FormLogin"
 
 export default function Home() {
-  const [viewportHeight, setViewportHeight] = useState("100vh");
-
-  useEffect(() => {
-    const adjustHeight = () => {
-      setViewportHeight(`${window.innerHeight}px`);
-    }
-
-    window.addEventListener("resize", adjustHeight);
-    adjustHeight();
-
-    return () => window.removeEventListener("resize", adjustHeight);
-  }, []);
 
   return (
     <div
-      className="flex w-full flex-col items-center justify-start lg:justify-center bg-darkBlue lg:bg-transparent imageBackground overflow-hidden"
-      style={{ height: viewportHeight }} 
-    >
+      className="flex h-[calc(100dvh)] w-full flex-col items-center justify-start lg:justify-center bg-darkBlue lg:bg-transparent imageBackground overflow-hidden">
       <FormLogin />
     </div>
   )
