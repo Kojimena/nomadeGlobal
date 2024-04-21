611b0ba0ef97d4a1a4f6fd5d66126778a09226c7
Jimena Hernández <her21199@uvg.edu.gt>
2024-04-21 17:29:22 -0600

    Update FormLogin.jsx

--- components/FormLogin/FormLogin.jsx
+++ components/FormLogin/FormLogin.jsx
@@ -93,7 +93,7 @@ const FormLogin = () => {
             <div className="flex flex-col gap-4">
               <div className="sm:col-span-4">
                 <label  className="labelForm">
-                  Nombre de usuario o correo electrónico
+                  Usuario ó correo electrónico
                 </label>
                 <div className="mt-2">
                   <input
