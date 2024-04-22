e22f207eff2fc999daf7793b54542c0823245900
Jimena Hernández <her21199@uvg.edu.gt>
2024-04-22 12:42:56 -0600

    update

--- app/signupWorker/page.jsx
+++ app/signupWorker/page.jsx
@@ -5,7 +5,7 @@ import FormSignUp from '@/components/FormSignUp/FormSignUp'
 const SignUpWorker = () => {
   
   return (
-    <div className="flex h-[calc(100dvh)] w-full flex-col items-center justify-center overflow-hidden">
+    <div className="flex w-full flex-col items-center justify-center overflow-hidden">
         <FormSignUp />
     </div>
   )
